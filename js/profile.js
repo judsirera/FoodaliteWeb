b4fb7da414dd4cf9f2c31c343126fdd133ab2e57
Ashris Choudhury <ashris.me@gmail.com>
2017-11-07 17:05:53 -0500

    styling

--- js/profile.js
+++ js/profile.js
@@ -4,13 +4,13 @@ fbManager.downloadImages(user.id);
 
 function setGallery(src) {
     img = document.createElement('img');
-    img.className = 'materialboxed responsive-img';
+    img.className = 'materialboxed centered-and-cropped';
     img.src = src;
 
     div = document.createElement('div');
     div.className = 'img-gallery';
     div.appendChild(img);
-    
+
     gallery = document.getElementById('gallery');
     gallery.insertBefore(div, gallery.childNodes[0]);
 
